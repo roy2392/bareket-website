e024c018830d16646297b5979876eef92291929d
Roey Zalta <roey.zalta@gmail.com>
2024-11-01 15:32:34 +0200

    fix layout analytics part

--- bareket-website/app/layout.tsx
+++ bareket-website/app/layout.tsx
@@ -21,15 +21,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <head>
         <title>Next.js</title>
       </head>
-      <body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
         <Analytics />
       </body>
